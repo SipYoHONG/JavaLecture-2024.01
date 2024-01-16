4ea6b50c8ca568c0c53476d29dc0cd04e902bf2f
hong sipyo <qkqkqk1235@naver.com>
2024-01-16 11:00:28 +0900

    1111111
    1111

--- lesson/src/ch04_control/Ex01_control.java
+++
@@ -1,5 +0,0 @@
-package ch04_control;
-
-public class Ex01_control {
-
-}
---
+++ lesson/src/ch04_control/Ex02_Leapyear.java
@@ -0,0 +1,26 @@
+package ch04_control;
+
+import java.util.Scanner;
+
+public class Ex02_Leapyear {
+
+	public static void main(String[] args) {
+		System.out.println("연도 입력 >");
+		Scanner scan = new Scanner(System.in);
+		String inputStr = scan.nextLine();
+		int year = Integer.parseInt(inputStr);
+		scan.close();
+		// 4로 나누어지면 윤년, 100으로 나누어지면 평년, 400으로 나누어지면 윤년
+		
+		if(year % 400 == 0) {
+			System.out.println("윤년");
+		}else if(year % 100 == 0) {
+			System.out.println("평년");
+		}else if(year % 4 == 0) {
+			System.out.println("윤년");
+		}else {
+			System.out.println("평년");
+		}
+	}
+
+}
