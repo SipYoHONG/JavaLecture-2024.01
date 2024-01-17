94da1e8d86c3f4832f240a95e0ba16344d6521e2
hong sipyo <qkqkqk1235@naver.com>
2024-01-17 16:43:47 +0900

    1111
    111

--- lesson/src/ch05_reference/Q01.java
+++ lesson/src/ch05_reference/Q01.java
@@ -3,23 +3,16 @@ package ch05_reference;
 public class Q01 {
 
 	public static void main(String[] args) {
-		String numStr = "";
-		for(int i = 1; i <= 1000; i++) {
-			numStr += i;
-		}
-		System.out.println(numStr.replace("[^1]", "").length());
-		
-		for(int i = 0; i <= 9; i++) {
-			int count = numStr.length() - numStr.replaceAll(String.valueOf(i), "").length();
-            System.out.println(i + ": " + count);
-		}
-		int[] countArr = new int[10];
-		for (char c : numStr.toCharArray()) {
-			countArr[Character.getNumericValue(c)]++;
-		}
-		for (int i = 0; i < countArr.length; i++) {
-			System.out.println(i + ": " + countArr[i]);
+			String numStr = "";
+			for(int i = 1; i <= 1000; i++) {
+				numStr += i;
+			}
+			int[] countArr = new int[10];
+			for (char c : numStr.toCharArray()) {
+				countArr[Character.getNumericValue(c)]++;
+			}
+			for (int i = 0; i < countArr.length; i++) {
+				System.out.println(i + ": " + countArr[i]);
+			}
 		}
 	}
-
-}
