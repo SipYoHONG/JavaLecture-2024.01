b5c178b576d827499bd77e0d2289eab187fa1d32
hong sipyo <qkqkqk1235@naver.com>
2024-01-19 14:03:16 +0900

    다형성
    1111

---
+++ lesson/src/ch10_oop/sec04_polymorphism/Cat.java
@@ -0,0 +1,9 @@
+package ch10_oop.sec04_polymorphism;
+
+public class Cat extends Animal {
+	@Override
+	void sound() {
+		System.out.println("야옹~~");
+	}
+	void jump() { }
+}
---
+++ lesson/src/ch10_oop/sec04_polymorphism/Dog.java
@@ -0,0 +1,8 @@
+package ch10_oop.sec04_polymorphism;
+
+public class Dog extends Animal {
+	void sound() {
+		System.out.println("멍멍!!");
+	}
+	void walk() { } 
+}
